3af5c67f517414d8960003894e499ea0ac346854
Adam Cvikl <adam.wasd4@gmail.com>
2022-12-19 17:58:31 +0100

    implement basic args matching

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -4,7 +4,19 @@ mod lib;
 use clap::{Arg, ArgMatches, Command};
 
 fn main() {
+    match_args();
+}
+
+fn match_args() {
     let args = arguments();
+    match args.subcommand() {
+        Some(("import", set_matches)) => {}
+        Some(("pull", set_matches)) => {}
+        Some(("pull-all", set_matches)) => {}
+        Some(("push", set_matches)) => {}
+        Some(("push-all", set_matches)) => {}
+        _ => unreachable!(),
+    }
 }
 
 fn arguments() -> ArgMatches {
