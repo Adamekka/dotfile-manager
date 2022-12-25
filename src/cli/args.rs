6a0f36e36d1b0a97a8bfb8faf7c5f70c4022889e
Adam Cvikl <adam.wasd4@gmail.com>
2022-12-25 16:08:56 +0100

    add value_name to flags

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -9,24 +9,24 @@ pub fn arguments() -> ArgMatches {
         .subcommand(
             Command::new("import")
                 .about("Import a config")
-                .arg(Arg::new("git-path").short('g').long("git-path"))
-                .arg(Arg::new("path").short('p').long("path"))
-                .arg(Arg::new("name").short('n').long("name")),
+                .arg(Arg::new("git-path").short('g').long("git-path").value_name("git-path"))
+                .arg(Arg::new("path").short('p').long("path").value_name("path"))
+                .arg(Arg::new("name").short('n').long("name").value_name("name")),
         )
         .subcommand(
             Command::new("pull")
                 .about("Pull config from Git repo")
-                .arg(Arg::new("git-path").short('g').long("git-path"))
-                .arg(Arg::new("path").short('p').long("path"))
-                .arg(Arg::new("name").short('n').long("name")),
+                .arg(Arg::new("git-path").short('g').long("git-path").value_name("git-path"))
+                .arg(Arg::new("path").short('p').long("path").value_name("path"))
+                .arg(Arg::new("name").short('n').long("name").value_name("name")),
         )
         .subcommand(Command::new("pull-all").about("Pull all configs from Git repo(s)"))
         .subcommand(
             Command::new("push")
                 .about("Push config to Git repo")
-                .arg(Arg::new("git-path").short('g').long("git-path"))
-                .arg(Arg::new("path").short('p').long("path"))
-                .arg(Arg::new("name").short('n').long("name")),
+                .arg(Arg::new("git-path").short('g').long("git-path").value_name("git-path"))
+                .arg(Arg::new("path").short('p').long("path").value_name("path"))
+                .arg(Arg::new("name").short('n').long("name").value_name("name")),
         )
         .subcommand(Command::new("push-all").about("Push all configs to Git repo(s)"))
         .get_matches()
