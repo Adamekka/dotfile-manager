740b79d8f0b30af80c41f9bd2664b43599adefd3
Adam Cvikl <adam.wasd4@gmail.com>
2022-12-28 15:50:24 +0100

    make set_folders return config_folder

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 use std::{env, fs, path::Path};
 
-pub fn set_folders() {
+pub fn set_folders() -> String {
     // Check for config folder, else create one
     let home_folder = env::var("HOME").expect("$HOME environment variable isn't set");
     let config_folder = home_folder.clone() + "/.config/dotfile-manager";
-    let config_folder = Path::new(&config_folder);
+    let config_folder_path = Path::new(&config_folder);
 
-    if !config_folder.exists() {
-        fs::create_dir(config_folder).expect("Can't create '~/.config/dotfile-manager/")
+    if !config_folder_path.exists() {
+        fs::create_dir(config_folder_path).expect("Can't create '~/.config/dotfile-manager/")
     }
+
+    config_folder
 }
