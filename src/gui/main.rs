06fcd0e3f5f4049acbf2111c2f895bc5f1171884
Adam Cvikl <adam.wasd4@gmail.com>
2023-01-08 15:42:55 +0100

    create app window

--- src/gui/main.rs
+++ src/gui/main.rs
@@ -1,9 +1,32 @@
 #[path = "../lib.rs"]
 mod lib;
 
+use gtk::{prelude::{ApplicationExtManual, GtkWindowExt, ApplicationExt}, Application, ApplicationWindow};
 use lib::set_folders;
+
+const APP_ID: &str = "com.adamekka.dotfile-manager";
 
 fn main() {
     println!("dman-gui found on path");
     set_folders();
+
+    // Create new app
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of 'app'
+    app.connect_activate(build_ui);
+
+    // Run app
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Dotfile Manager by Adamekka")
+        .build();
+
+    // Present window
+    window.present();
 }
