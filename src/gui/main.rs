521228d93efceda4aa5f284d6468f355dd7f6f39
Adam Cvikl <adam.wasd4@gmail.com>
2023-01-08 16:28:21 +0100

    show title bar; set width, height

--- src/gui/main.rs
+++ src/gui/main.rs
@@ -1,7 +1,11 @@
 #[path = "../lib.rs"]
 mod lib;
 
-use gtk::{prelude::{ApplicationExtManual, GtkWindowExt, ApplicationExt}, Application, ApplicationWindow};
+use gtk::{
+    gtk::{Box, Button, Orientation},
+    prelude::{ApplicationExt, ApplicationExtManual, BoxExt, ButtonExt, GtkWindowExt, WidgetExt},
+    Application, ApplicationWindow, HeaderBar, WindowTitle,
+};
 use lib::set_folders;
 
 const APP_ID: &str = "com.adamekka.dotfile-manager";
@@ -21,10 +25,26 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
+    // let button = Button::builder().label("press").build();
+
+    // button.connect_clicked(move |button| button.set_label("clicked"));
+
+    // Title bar
+    let title_bar = Box::new(Orientation::Vertical, 0);
+    title_bar.append(
+        &HeaderBar::builder()
+            .title_widget(&WindowTitle::new("Dotfile Manager by Adamekka", ""))
+            .build(),
+    );
+
     // Create a window
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Dotfile Manager by Adamekka")
+        .default_height(480)
+        .default_width(640)
+        .content(&title_bar)
+        // .content(&button)
         .build();
 
     // Present window
