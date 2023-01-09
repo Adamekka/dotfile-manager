8aecb76e8637c9be3ea64aec3b8355e55c084b84
Adam Cvikl <adam.wasd4@gmail.com>
2023-01-09 17:06:26 +0100

    start trying to make templates list

--- src/gui/main.rs
+++ src/gui/main.rs
@@ -2,11 +2,14 @@
 mod lib;
 
 use gtk::{
-    gtk::{Box, Button, Orientation},
+    glib::clone,
+    glib::clone::{Downgrade, Upgrade},
+    gtk::{Box, Button, ListBox, Orientation, SelectionMode},
     prelude::{ApplicationExt, ApplicationExtManual, BoxExt, ButtonExt, GtkWindowExt, WidgetExt},
-    Application, ApplicationWindow, HeaderBar, WindowTitle,
+    ActionRow, Application, ApplicationWindow, HeaderBar, WindowTitle,
 };
 use lib::set_folders;
+use std::{cell::Cell, rc::Rc};
 
 const APP_ID: &str = "com.adamekka.dotfile-manager";
 
@@ -25,17 +28,63 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
-    // let button = Button::builder().label("press").build();
+    let first_row = ActionRow::builder()
+        .activatable(true)
+        .title("first row")
+        .build();
 
-    // button.connect_clicked(move |button| button.set_label("clicked"));
+    let templates_list = ListBox::builder()
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .selection_mode(SelectionMode::None)
+        .css_classes(vec![String::from("boxed-list")])
+        .build();
+    templates_list.append(&first_row);
+
+    // let button_increase = Button::builder()
+    //     .label("increase")
+    //     .margin_top(12)
+    //     .margin_bottom(12)
+    //     .margin_start(256)
+    //     .margin_end(256)
+    //     .build();
+
+    // let button_decrease = Button::builder()
+    //     .label("decrease")
+    //     .margin_top(12)
+    //     .margin_bottom(12)
+    //     .margin_start(256)
+    //     .margin_end(256)
+    //     .build();
+
+    // let count = Rc::new(Cell::new(0));
+
+    // button_increase.connect_clicked(clone!(@strong count, @strong button_decrease =>
+    //     move |_| {
+    //         count.set(count.get() + 1);
+    //         button_decrease.set_label(&count.get().to_string());
+    // }));
+    // button_decrease.connect_clicked(clone!(@strong button_increase =>
+    //     move |_| {
+    //         count.set(count.get() - 1);
+    //         button_increase.set_label(&count.get().to_string());
+    // }));
 
     // Title bar
     let title_bar = Box::new(Orientation::Vertical, 0);
     title_bar.append(
         &HeaderBar::builder()
-            .title_widget(&WindowTitle::new("Dotfile Manager by Adamekka", ""))
+            .title_widget(&WindowTitle::new("Dotfile Manager", "by Adamekka"))
             .build(),
     );
+
+    let gtk_box = Box::builder().orientation(Orientation::Vertical).build();
+    gtk_box.append(&title_bar);
+    // gtk_box.append(&button_increase);
+    // gtk_box.append(&button_decrease);
+    gtk_box.append(&templates_list);
 
     // Create a window
     let window = ApplicationWindow::builder()
@@ -43,8 +92,7 @@ fn build_ui(app: &Application) {
         .title("Dotfile Manager by Adamekka")
         .default_height(480)
         .default_width(640)
-        .content(&title_bar)
-        // .content(&button)
+        .content(&gtk_box)
         .build();
 
     // Present window
