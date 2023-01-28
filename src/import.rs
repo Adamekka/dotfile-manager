6c832823a76f352cb5734a16ed5829cb78b977e8
Adam Cvikl <adam.wasd4@gmail.com>
2023-01-28 22:32:01 +0100

    put `templates` to vector

--- src/import.rs
+++ src/import.rs
@@ -1,3 +1,6 @@
+mod lib;
+
+use lib::Template;
 use std::path::Path;
 
 /// Import templates from a file
@@ -31,8 +34,61 @@ pub fn import_templates(file_path: String) {
         }
     }
 
-    // check if file is a valid toml file
-    // check if file contains valid template(s)
+    let file = std::fs::read_to_string(file_path).unwrap();
+    let file_contents: toml::Value = toml::from_str(&file).unwrap();
+    let mut templates: Vec<Template> = Vec::new();
+
+    // put templates to vector
+    for (key, value) in file_contents.as_table().unwrap().iter() {
+        #[cfg(debug_assertions)]
+        {
+            println!("{key}: {value}");
+        }
+
+        // check if file contains valid values
+
+        let name = match value.get("name") {
+            Some(name) => name,
+            None => {
+                panic!("Template does not contain a name");
+            }
+        };
+
+        let path = match value.get("path") {
+            Some(path) => path,
+            None => {
+                panic!("Template does not contain a path");
+            }
+        };
+
+        let git_path = match value.get("git_path") {
+            Some(git_path) => git_path,
+            None => {
+                panic!("Template does not contain a git_path");
+            }
+        };
+
+        #[cfg(debug_assertions)]
+        {
+            println!("name: {name}");
+            println!("path: {path}");
+            println!("git_path: {git_path}");
+        }
+
+        let template = Template {
+            name: name.to_string(),
+            path: path.to_string(),
+            git_path: git_path.to_string(),
+        };
+
+        templates.push(template);
+    }
+
+    #[cfg(debug_assertions)]
+    {
+        println!("templates: {templates:?}");
+    }
+
     // check if template(s) already exists
     // import template(s)
 
