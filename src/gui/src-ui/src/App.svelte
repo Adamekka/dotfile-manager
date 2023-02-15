89118f624bf1ee333e390f920e582c0e27374ac7
Adam Cvikl <adam.wasd4@gmail.com>
2023-02-15 17:50:19 +0100

    `Tauri`: render Template list

--- src/gui/src-ui/src/App.svelte
+++ src/gui/src-ui/src/App.svelte
@@ -7,15 +7,25 @@
     git_path: String;
   }
 
+  let templates: Array<Template> = [];
+
   function reload_templates() {
-    invoke("reload_templates").then((result) => {
-      console.log(result);
+    invoke("reload_templates").then((result: Array<Template>) => {
+      console.table(result);
+      templates = result;
     });
   }
 </script>
 
 <main class="container">
   <button on:click={reload_templates}>Reload templates</button>
+  {#each templates as template}
+    <div>
+      <h1>{template[0]}</h1>
+      <p>{template[1]}</p>
+      <p>{template[2]}</p>
+    </div>
+  {/each}
 </main>
 
 <style>
