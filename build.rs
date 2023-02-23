23258d790bfaa455ca738da9ab1fc50877070248
Adam Cvikl <adam.wasd4@gmail.com>
2023-02-23 20:07:06 +0100

    create git_commit_hash env variable during build for versions

--- build.rs
+++ build.rs
@@ -1,7 +1,20 @@
 #[cfg(feature = "gui")]
 fn main() {
+    add_git_commit_hash();
     tauri_build::build();
 }
 
 #[cfg(not(feature = "gui"))]
-fn main() {}
+fn main() {
+    add_git_commit_hash();
+}
+
+fn add_git_commit_hash() {
+    let git_commit_hash = std::process::Command::new("git")
+        .args(&["rev-parse", "--short", "HEAD"])
+        .output()
+        .expect("Failed to get git commit hash")
+        .stdout;
+    let git_commit_hash = String::from_utf8(git_commit_hash).unwrap();
+    println!("cargo:rustc-env=DMAN_GIT_COMMIT_HASH={}", git_commit_hash);
+}
