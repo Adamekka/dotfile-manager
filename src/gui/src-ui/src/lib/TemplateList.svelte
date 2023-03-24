0dd4de3caa50354f80da1f1d9b7f874775e4bb61
Adam Cvikl <adam.wasd4@gmail.com>
2023-03-24 17:22:33 +0100

    `Tauri`: `templates` is now Array of objects

--- src/gui/src-ui/src/lib/TemplateList.svelte
+++ src/gui/src-ui/src/lib/TemplateList.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/tauri";
 
-  interface Template {
-    name: String;
-    path: String;
-    git_path: String;
-  }
-
-  let templates: Array<Template> = [];
+  let templates: { name: string; path: string; git_path: string }[] = [];
 
   function reload_templates() {
-    invoke("reload_templates").then((result: Array<Template>) => {
+    invoke("reload_templates").then((result: Array<string>) => {
+      // Clear the array before adding new items to it to avoid duplicates
+      templates = [];
       console.table(result);
-      templates = result;
+      // Add each template to the array of objects
+      result.forEach(function (template) {
+        let name = template[0];
+        let path = template[1];
+        let git_path = template[2];
+        templates.push({ name, path, git_path });
+      });
     });
   }
 </script>
@@ -20,8 +22,8 @@
 <button on:click={reload_templates}>Reload templates</button>
 {#each templates as template}
   <li>
-    <h1>{template[0]}</h1>
-    <p>{template[1]}</p>
-    <p>{template[2]}</p>
+    <h1>{template.name}</h1>
+    <p>{template.path}</p>
+    <p>{template.git_path}</p>
   </li>
 {/each}
