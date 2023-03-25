ba8cad869f214db51d08be321007460f16a93e41
Adam Cvikl <adam.wasd4@gmail.com>
2023-03-25 21:29:39 +0100

    bind `selected_template` between components

--- src/gui/src-ui/src/App.svelte
+++ src/gui/src-ui/src/App.svelte
@@ -1,7 +1,25 @@
 <script lang="ts">
   import TemplateList from "./lib/TemplateList.svelte";
+  import TemplateInfo from "./lib/TemplateInfo.svelte";
+
+  let selected_template: {
+    name: string;
+    path: string;
+    git_path: string;
+  } = {
+    name: "None",
+    path: "None",
+    git_path: "None",
+  };
 </script>
 
 <main class="container">
-  <TemplateList />
+  <div class="flex">
+    <div class="flex-1">
+      <TemplateList bind:selected_template />
+    </div>
+    <div class="flex-1">
+      <TemplateInfo bind:selected_template />
+    </div>
+  </div>
 </main>
---
+++ src/gui/src-ui/src/lib/TemplateInfo.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let selected_template: {
+    name: string;
+    path: string;
+    git_path: string;
+  } = {
+    name: "None",
+    path: "None",
+    git_path: "None",
+  };
+</script>
+
+<h2 class="font-bold text-center text-2xl">
+  Selected template: {selected_template.name}
+</h2>
+<!-- <button class="btn">Pull</button>
+<button class="btn">Push</button> -->
