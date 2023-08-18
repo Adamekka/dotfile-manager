4e55566094987ceb44062154ae5e5ebac2b7e4af
Adam Cvikl <adam.wasd4@gmail.com>
2023-08-18 17:32:27 +0200

    cleanup

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
         };
 
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo pkg-config ];
+          nativeBuildInputs = with pkgs; [ pkg-config ];
           PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
         };
       }
