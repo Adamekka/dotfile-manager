c9fe07a7fb0bb5f248176cdee52c805d4b232d1d
Adam Cvikl <adam.wasd4@gmail.com>
2023-08-22 20:57:55 +0200

    add `rustup` to `shell.nix`

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
+# VS Code is using this
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ pkg-config ];
+  nativeBuildInputs = with pkgs; [ pkg-config rustup ];
   PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 }
